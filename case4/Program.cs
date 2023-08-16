bfa922dd4a87cfeb35faf1382846575f10273b0a
Sergei <radchenkovsa@yandex.ru>
2023-08-16 15:31:09 +0300

    case4

--- case4/Program.cs
+++ case4/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+bool Multiplicity(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+int num = InputNum("Введите число: ");
+bool result = Multiplicity(num);
+
+if(result)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
