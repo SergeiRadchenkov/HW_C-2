273413d070db8145c875e74c9aae96a441eac5d9
Sergei <radchenkovsa@yandex.ru>
2023-08-16 14:41:12 +0300

    case3

--- case3/Program.cs
+++ case3/Program.cs
@@ -1,4 +1,14 @@
-﻿Console.Write("Введите первое число: ");
+﻿/*Console.Write("Введите первое число: ");
 int firstNum = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
-int secondNum = int.Parse(Console.ReadLine()!);
+int secondNum = int.Parse(Console.ReadLine()!); */
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    // int num = int.Parse(Console.ReadLine()!);
+    // return = int num;
+    return int.Parse(Console.ReadLine()!);
+}
+int firstNum = InterNum("Введите первое число: ");
+int secondNum = InterNum("Введите второе число: ");
